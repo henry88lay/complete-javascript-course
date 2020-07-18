0e541cfce0a4af62c5cafb6107db12fae979eb18
henry lay <henry88lay@gmail.com>
2020-07-18 19:18:25 +1000

    started working on the first part of javascript on the project

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,3 +8,33 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+// Declaring Our Variables
+let scores, roundScore, activePlayer;
+
+// Defining Our Variables
+scores = [0,0];
+roundScore = 0;
+activePlayer = 1;
+
+
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  // 1. Random Number
+  let dice = Math.floor(Math.random() * 6) + 1;
+  // 2. Display the result
+  let diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = `dice-${dice}.png`
+  // 3. Update the round score IF the rolled number NOT a 1
+})
