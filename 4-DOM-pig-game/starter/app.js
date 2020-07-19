efab540b8893f9be2b0a80626ac2b4be4687331f
henry lay <henry88lay@gmail.com>
2020-07-19 13:49:00 +1000

    debuged and retested the code with the init function

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -11,20 +11,12 @@ GAME RULES:
 
 // Declaring Our Variables
 let scores, roundScore, activePlayer;
-  scores = [0,0];
-  roundScore = 0;
-  activePlayer = 0;
+
+init();
 
 const hideDice = document.querySelector('.dice').style.display = 'none'; 
 
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-hideDice
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-
 
 document.querySelector('.btn-roll').addEventListener('click', () => {
   // 1. Random Number
@@ -36,7 +28,7 @@ document.querySelector('.btn-roll').addEventListener('click', () => {
   // 3. Update the round score IF the rolled number NOT a 1
     if (dice !== 1) {
       roundScore += dice;
-      document.querySelector(`#current-${activePlayer}`).textContent = scores[active];
+      document.querySelector(`#current-${activePlayer}`).textContent = scores[activePlayer];
     } else {
       nextPlayer();
     }
@@ -73,4 +65,19 @@ let nextPlayer = () => {
   document.querySelector('.player-1-panel').classList.toggle('active');
 
   hideDice
+};
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+  scores = [0,0];
+  roundScore = 0;
+  activePlayer = 0;
+  document.querySelector('.dice').style.display = 'none'; 
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
 };
